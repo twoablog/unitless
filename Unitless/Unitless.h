027ba50a413d7137d62b4c700f20e94eff8041e4
Tomas Silveira Salles <ts@imagineon.de>
2018-08-02 17:57:20 +0200

    Added xcode project.

---
+++ Unitless/Unitless.h
@@ -0,0 +1,19 @@
+//
+//  Unitless.h
+//  Unitless
+//
+//  Created by Tomás Silveira Salles on 02.08.18.
+//  Copyright © 2018 TwoA. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Unitless.
+FOUNDATION_EXPORT double UnitlessVersionNumber;
+
+//! Project version string for Unitless.
+FOUNDATION_EXPORT const unsigned char UnitlessVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Unitless/PublicHeader.h>
+
+
