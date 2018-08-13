a0505cc38dfb3a41678853b61382efa153080de1
Tomas Silveira Salles <ts@imagineon.de>
2018-08-13 09:54:24 +0200

    Temperature: Added `Temperature` protocol.

---
+++ Unitless/Temperature/Temperature.swift
@@ -0,0 +1,29 @@
+//
+//  Temperature.swift
+//  Unitless
+//
+//  Created by Tomás Silveira Salles on 13.08.18.
+//  Copyright © 2018 TwoA. All rights reserved.
+//
+
+import Foundation
+
+public protocol Temperature: Comparable, Hashable {
+	// MARK: Conversions
+	
+	var kelvin: Double { get }
+	var celsius: Double { get }
+	var fahrenheit: Double { get }
+	
+	// MARK: Initializers
+	
+	init(kelvin: Double)
+	init(celsius: Double)
+	init(fahrenheit: Double)
+	
+	// MARK: Constants
+	
+	static var absoluteZero: Self { get }
+	static var waterFreezingPoint: Self { get }
+	static var waterBoilingPoint: Self { get }
+}
---
+++ UnitlessTests/Temperature/TemperatureTests.swift
@@ -0,0 +1,9 @@
+//
+//  TemperatureTests.swift
+//  UnitlessTests
+//
+//  Created by Tomás Silveira Salles on 13.08.18.
+//  Copyright © 2018 TwoA. All rights reserved.
+//
+
+import Foundation
