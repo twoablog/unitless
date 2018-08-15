4b64bb19bcf4fffda9040776fbd5c23434788fcb
Tomas Silveira Salles <ts@imagineon.de>
2018-08-15 13:39:45 +0200

    Temperature: Added simple formatter (protocol and implementation) for string conversion.

---
+++ Unitless/Temperature/TemperatureFormatter.swift
@@ -0,0 +1,30 @@
+//
+//  TemperatureFormatter.swift
+//  Unitless
+//
+//  Created by Tomás Silveira Salles on 15.08.18.
+//  Copyright © 2018 TwoA. All rights reserved.
+//
+
+import Foundation
+
+public struct TemperatureFormatter: TemperatureFormatterProtocol {
+	public enum Format {
+		case kelvin
+		case celsius
+		case fahrenheit
+	}
+	
+	public let format: Format
+	
+	public func toString<T>(_ temperature: T) -> String where T : TemperatureProtocol {
+		switch format {
+		case .kelvin:
+			return "\(temperature.kelvin)K"
+		case .celsius:
+			return "\(temperature.celsius)°C"
+		case .fahrenheit:
+			return "\(temperature.fahrenheit)°F"
+		}
+	}
+}
